f5545f6a0ecbaa0ec681403c7b4fb5ade0fe3d8a
TanjaM <malic.tanja@gmail.com>
2015-01-09 19:38:52 +0100

    extended for ploting several splines on one graph

--- PlotSplines/plotSpline.m
+++ PlotSplines/plotSpline.m
@@ -1,16 +1,32 @@
-function plotSpline(input, accuracy)
+%function usage example:
+%  s1 = '[(1,[0,1])]'
+%  s2 = '[(1,[1,-1])]'
+%  s3 = ...
+%  s={s1,s2, s3, ...}
+%  plotSpline(s)    ... ploting several splines
+%  plotSpline({s1}) ... plot one spline
+
+function plotSpline(splineList)
     if nargin==1
-        accuracy=0.000001;
+        accuracy=0.001;
     end
     figure(1); clf; hold on;
+    n = length(splineList);
+    colors = ['b','r','g','y','m','c','w','k'];
+    for i=1:n
+       plotingSpline = i
+       plotPolySegment(char(splineList(i)), accuracy, colors(mod(i,length(colors)))) ;
+    end
+end
+
+function plotPolySegment(input, accuracy, color)
     delimitedSpline =  regexp(input,'(','split');
     n = length(delimitedSpline);
-    numSplines = n-1
+    numPolySegments = n-1
     breakpoint=0;
     for i=2:n
        j = char(delimitedSpline(i));
        polySegment = regexp(j,',[','split');
-       polySegment(1);
        duration=str2double(char(polySegment(1)));
        poly=char(polySegment(2));
        k=size(poly,2);
@@ -22,11 +38,11 @@ function plotSpline(input, accuracy)
        end
        coefficients=zeros(1,length(c));
        for p=1:length(c)
-          coefficients(length(c) - p + 1) = str2double(char(c(p)));
+           coefficients(length(c)+1-p)= str2double(char(c(p)));
        end
        x=breakpoint:accuracy:(duration+breakpoint);
        y=polyval(coefficients, 0:accuracy:duration);
-       plot(x,y);
+       plot(x,y,'Color',color);
        breakpoint=breakpoint+duration;
     end
     splineEnd = breakpoint
